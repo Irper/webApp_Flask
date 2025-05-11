fe4c4b16e72c32fa4799645c78c1eac973b3dd27
Vova <irper02@bk.ru>
2025-05-11 15:06:14 +1000

    New project

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def hello_world():
+  return render_template('/hello.html', name=__name__)
+
+if __name__ == '__main__':
+  app.run(debug=True)
