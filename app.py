cb0cef8ef69be9167b3fb78304fe1e28fb5d3d7a
Vova <irper02@bk.ru>
2025-05-22 14:20:37 +1000

    changes...

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ app.config['SESSION_COOKIE_SECURE'] = False
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 app.config['PERMANENT_SESSION_LIFETIME'] = 1800  # 30 минут
-app.config['PREFERRED_URL_SCHEME'] = 'https'
 
 # Инициализация расширений
 db = SQLAlchemy(app)
